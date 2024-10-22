8149e7f9e47d6fa49b41a9da90db01ee1c451b5d
dipakPjs <deepakpokharel811@gmail.com>
2024-10-23 00:19:17 +0545

    some updates

--- src/slices/ContactBlock/index.tsx
+++ src/slices/ContactBlock/index.tsx
@@ -131,7 +131,7 @@ const ContactBlock = ({ slice }: ContactBlockProps): JSX.Element => {
               <button
                 type="submit"
                 value="send"
-                className="group relative mx-auto flex w-fit items-center justify-center overflow-hidden rounded-[55px] border-2 border-slate-900 bg-slate-50 px-10 py-2 font-bold text-slate-900 transition-transform ease-out hover:scale-105"
+                className="group relative mx-auto flex w-fit items-center justify-center overflow-hidden rounded-[55px] border-2 border-slate-900 bg-slate-50 px-10 py-2 font-bold text-slate-900 transition-transform duration-700 ease-in-out hover:text-white hover:scale-105"
               >
                 <span className="h-ful absolute inset-0 z-0 translate-y-9 rounded-[50px] bg-[#0037ff] transition-transform duration-300 ease-in-out group-hover:translate-y-0" />
                 <span className="relative flex items-center justify-center gap-2">
